920a8d4adbdc3e1b3bef1e9f89d4c832b1374263
Guacanole01 <98541203+Guacanole01@users.noreply.github.com>
2025-02-17 19:21:32 +0000

    added Butler's translation

--- tlg0012.tlg001.perseus-eng4.pope-alignments:1.40-1.44.py
+++ tlg0012.tlg001.perseus-eng4.pope-alignments:1.40-1.44.py
@@ -1,1 +1,9 @@
-
+alignments = {
+    "1.40-1.44": {
+        "butler": """or burned for you thigh-bones in fat of bulls or
+                     goats, grant my prayer, and let your arrows avenge these my tears upon the
+                     Danaans." Thus did he pray, and Apollo heard his prayer. He came down furious
+                     from the summits of"""
+        "pope": """test"""
+    }
+}
