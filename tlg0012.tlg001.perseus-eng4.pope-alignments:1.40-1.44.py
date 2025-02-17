51980ee4a94a8cb13162d62299240c1617b13092
Guacanole01 <98541203+Guacanole01@users.noreply.github.com>
2025-02-17 19:41:36 +0000

    Added the Pope's translation

--- tlg0012.tlg001.perseus-eng4.pope-alignments:1.40-1.44.py
+++ tlg0012.tlg001.perseus-eng4.pope-alignments:1.40-1.44.py
@@ -3,7 +3,11 @@ alignments = {
         "butler": """or burned for you thigh-bones in fat of bulls or
                      goats, grant my prayer, and let your arrows avenge these my tears upon the
                      Danaans." Thus did he pray, and Apollo heard his prayer. He came down furious
-                     from the summits of"""
-        "pope": """test"""
+                     from the summits of""",
+        "pope": """And prayers, and tears, and bribes, shall plead in vain;
+Till time shall rifle every youthful grace,
+And age dismiss her from my cold embrace,
+In daily labours of the loom employ’d,
+Or doom’d to deck the bed she once enjoy’d."""
     }
 }
