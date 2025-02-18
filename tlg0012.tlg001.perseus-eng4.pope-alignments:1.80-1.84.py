9263c415ad0a5ab43a74e46ffc7bd5fbd38d5ad5
Linh Nguyen <137933787+lnguyen2693@users.noreply.github.com>
2025-02-18 19:36:11 +0000

    hw

---
+++ tlg0012.tlg001.perseus-eng4.pope-alignments:1.80-1.84.py
@@ -0,0 +1,17 @@
+alignments = {
+    "1.80-1.84": {
+        "butler": '''A plain man cannot stand against the anger of a
+                     king, who if he swallow his displeasure now, will yet nurse revenge till he has
+                     wreaked it. Consider, therefore, whether or no you will protect me." And
+                     Achilles answered,''',
+        "pope": '''
+        And measure back the seas we cross’d before?
+The plague destroying whom the sword would spare,
+’Tis time to save the few remains of war.
+But let some prophet, or some sacred sage,
+Explore the cause of great Apollo’s rage;
+Or learn the wasteful vengeance to remove
+By mystic dreams, for dreams descend from Jove.
+        '''
+    }
+}
