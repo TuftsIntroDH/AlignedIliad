a32326981e654fd1df0cf3bb888966b31b0430f9
Kalyn Muhlenberg <123048761+kmuhle@users.noreply.github.com>
2025-02-19 03:05:40 +0000

    1.75-1.79

---
+++ tlg0012.tlg001.perseus-eng4.pope-alignments:1.75-1.79.py
@@ -0,0 +1,15 @@
+alignments = {
+    "1.75-1.79": {
+        "butler": """anger [<term xml:lang="grc">mênis</term>] of
+King Apollo, I will therefore do so; but consider first and swear that you will
+stand by me heartily in word and deed, for I know that I shall offend one who
+rules the Argives with might, to whom all the Achaeans are in subjection.""",
+        "pope": """Why angry Phœbus bends his fatal bow?
+First give thy faith, and plight a prince's word
+Of sure protection, by thy power and sword:
+For I must speak what wisdom would conceal,
+And truths, invidious to the great, reveal,
+Bold is the task, when subjects, grown too wise,
+Instruct a monarch where his error lies;"""
+    }
+}
