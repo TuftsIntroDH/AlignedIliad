fa70a1e15305d019e20f1f41308471ba3b032835
JacobBenSchwartz <jacob.schwartz1@tufts.edu>
2025-02-19 19:51:54 +0000

    Created dictionaries

---
+++ tlg0012.tlg001.perseus-eng4.pope-alignments:1.5-1.9.py
@@ -0,0 +1,23 @@
+alignments = {
+    "1.5-1.9" = {
+        "butler":"""and vultures, for so was the will of Zeus
+fulfilled from the day on which the son of Atreus, king of men, and great
+Achilles, first fell out with one another. And which of the gods was it that
+set them on to quarrel? It was the son of Zeus and Leto; for he was angry with
+the king"""
+        "pope":"""Devouring dogs and hungry vultures tore.[41]
+Since great Achilles and Atrides strove,
+Such was the sovereign doom, and such the will of Jove![42]
+Declare, O Muse! in what ill-fated hour[43]
+Sprung the fierce strife, from what offended power
+Latona’s son a dire contagion spread,[44]
+And heap’d the camp with mountains of the dead;
+The king of men his reverent priest defied,[45]
+And for the king’s offence the people died."""
+        "homer":"""οἰωνοῖσί τε πᾶσι, Διὸς δ᾽ ἐτελείετο βουλή,
+ἐξ οὗ δὴ τὰ πρῶτα διαστήτην ἐρίσαντε
+Ἀτρεΐδης τε ἄναξ ἀνδρῶν καὶ δῖος Ἀχιλλεύς.
+τίς τ᾽ ἄρ σφωε θεῶν ἔριδι ξυνέηκε μάχεσθαι;
+Λητοῦς καὶ Διὸς υἱός: ὃ γὰρ βασιλῆϊ χολωθεὶς"""
+    }
+}
