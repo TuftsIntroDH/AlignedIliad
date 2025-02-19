b3625db8eaf2b637d92b21d622c890da1b9e07b1
justin1iu <justin.liu@tufts.edu>
2025-02-18 20:03:51 -0500

    Update tlg0012.tlg001.perseus-eng4.pope-alignments:1.65-1.69.py

--- tlg0012.tlg001.perseus-eng4.pope-alignments:1.65-1.69.py
+++ tlg0012.tlg001.perseus-eng4.pope-alignments:1.65-1.69.py
@@ -1,1 +1,13 @@
-
+alignments = {
+    "1.65–1.69": {
+        "butler": """whether it is for some vow that we have broken,
+                     or hecatomb that we have not offered, and whether he will accept the savor of
+                     lambs and goats without blemish, so as to take away the plague from us." With
+                     these words he sat down, and Kalkhas son of Thestor, wisest of augurs, """,
+        "pope": """\"Hence on thy life, and fly these hostile plains,
+Nor ask, presumptuous, what the king detains:
+Hence, with thy laurel crown, and golden rod,
+Nor trust too far those ensigns of thy god.
+Mine is thy daughter, priest, and shall remain;"\"""
+    }
+}
