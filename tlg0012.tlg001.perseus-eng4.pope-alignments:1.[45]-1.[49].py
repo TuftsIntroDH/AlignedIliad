c7cc0101951287a5d2e696c7dd2081ca165e4796
ckean02 <catie.kean@tufts.edu>
2025-02-19 03:58:16 +0000

    45-49

---
+++ tlg0012.tlg001.perseus-eng4.pope-alignments:1.[45]-1.[49].py
@@ -0,0 +1,9 @@
+alignments = {
+    "1.45–1.49": {
+        "butler": """with his bow and his quiver upon his shoulder,
+and the arrows rattled on his back with the rage that trembled within him. He
+sat himself down away from the ships with a face as dark as night, and his
+silver bow rang death as he shot his arrow in the midst of them.""",
+        "pope": """The arrows rattled on the shoulders of the angry god as he moved, and his coming was like the night.  Then he sat down apart from the ships and let fly an arrow: terrible was the twang of the silver bow.  The mules he assailed first and the swift dogs"""
+           }
+}
