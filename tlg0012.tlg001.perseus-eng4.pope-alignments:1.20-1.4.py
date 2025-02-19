559a2b4358e43de540f32dcae12ace816c4ddc9a
laurenlangbort <119360535+laurenlangbort@users.noreply.github.com>
2025-02-19 04:12:49 +0000

    create dictionary called alignments

--- tlg0012.tlg001.perseus-eng4.pope-alignments:1.20-1.4.py
+++ tlg0012.tlg001.perseus-eng4.pope-alignments:1.20-1.4.py
@@ -1,1 +1,14 @@
-
+alignments = {
+    "1.10-1.14": {
+        "butler": """and sent a pestilence upon the host to plague
+                     the people, because the son of Atreus had dishonored Chryses his priest. Now
+                     Chryses had come to the ships of the Achaeans to free his daughter, and had
+                     brought with him a great ransom: moreover he bore in his hand the scepter of
+                     Apollo wreathed with a suppliant's wreath """,
+        "pope": """Sprung the fierce strife, from what offended power
+Latona's son a dire contagion spread,
+And heap'd the camp with mountains of the dead;
+The king of men his reverent priest defied,
+And for the king's offence the people died."""
+    }
+}
